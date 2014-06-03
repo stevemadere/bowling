81292ba42dd98ef5aa5f2300f62d633c00757e06
Steve Madere <steve@stevemadere.com>
2014-06-03 10:06:09 -0500

    Initial state from command "rails new bowling"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bowling::Application.config.secret_token = '47a848d81182eadb2b8c83bb498d44b978e7c88d770dcab298ba3a5308adc8021b3c9a16dbae042e273a075df10666f64628303a6cf1d933ccb897e5a5cb271f'
