545417525d7a349d4e01647617693039e935c83c
Steve Madere <steve@stevemadere.com>
2014-06-03 13:51:37 -0500

    added basic GET show test

--- spec/controllers/players_controller_spec.rb
+++ spec/controllers/players_controller_spec.rb
@@ -20,10 +20,20 @@ describe PlayersController, :controllers do
 
   describe "GET index" do
     Given!(:created_players) { create_list(:player, 3) }
-    Given!(:players_in_db) { Player.all.to_a }
+    Given!(:players_from_db) { Player.all.to_a }
     When(:response) { get(:index, format: :json) }
     Then { response.status.should eq(200) }
-    And { (Set.new((JSON.parse response.body).map {|p| [p["id"],p["name"]]})).should eq(Set.new(players_in_db.map {|p| [p.id,p.name]})) }
+    And { (Set.new((JSON.parse response.body).map {|p| [p["id"],p["name"]]})).should eq(Set.new(players_from_db.map {|p| [p.id,p.name]})) }
+  end
+
+  describe "GET show",:now do
+    Given!(:created_players) { create_list(:player, 3) }
+    Given!(:players_from_db) { Player.all.to_a }
+    Given(:chosen_player) { players_from_db[2] }
+    When(:response) { get(:show, {id: chosen_player.id, format: :json}) }
+    Then { response.status.should eq(200) }
+    And { (JSON.parse response.body)["name"].should eq(chosen_player.name) }
+    And { (JSON.parse response.body)["id"].should eq(chosen_player.id) }
   end
 
 
