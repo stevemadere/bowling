8bc0dc14a97d5e38ca0423893ac33e24a5bb138a
Steve Madere <steve@stevemadere.com>
2014-06-04 12:30:43 -0500

    ensuring that destroyed Players or Games clean up associated PlayerGame instances

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ActiveRecord::Base
   # attr_accessible :title, :body
-  has_many :player_games
+  has_many :player_games, :dependent => :destroy
   has_many :players, :through => :player_games
 
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,6 @@
 class Player < ActiveRecord::Base
   attr_accessible :name
 
-  has_many :player_games
+  has_many :player_games, :dependent => :destroy
   has_many :games, :through => :player_game
 end
